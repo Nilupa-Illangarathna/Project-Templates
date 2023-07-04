c76cbf8d656a444ca4dae504952a29b10584d87f
Nilupa Illangarathna <nilupalakshantha50@gmail.com>
2023-07-04 08:48:02 +0530

    template codes are added

---
+++ maven-java-template/src/main/java/com/example/Athena_SDK_Main.java
@@ -0,0 +1,15 @@
+package com.example;
+
+
+
+public class Athena_SDK_Main {
+
+    public static void main(String[] args) {
+//        Config config = ConfigReader.readConfig();
+//
+//        // Access the configuration properties
+        System.out.println("Hello world");
+    }
+
+
+}
---
+++ react-template/src/index.js
@@ -0,0 +1,16 @@
+const React =require("react");
+const ReactDOM = require("react-dom");
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+ReactDOM.render(<h1>Hello World</h1> , document.getElementById("root"));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
